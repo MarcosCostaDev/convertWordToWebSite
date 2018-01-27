4823e2760b9a9b7da37a6a66b87e00d3c6bed7f9
Marcos Costa <marcos.lcosta@live.com>
2018-01-27 13:23:37 -0300

    organizando tarefas

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const configGlobal = require("./app/config/configGlobal"),
     gulp = require("gulp"),
     mammoth = require("mammoth"),
     through = require("through2"),
-    prepareFiles = require("./app/prepareFiles"),
     converter = require("./app/converter")
 
 let caminhos = [
@@ -11,13 +10,25 @@ let caminhos = [
 ]
 
 
-gulp.task("default", function () {
+gulp.task("default", ["createWiki"], function () {
 
-    prepareFiles.createDist(configGlobal.configuracao.dist, false);
+
+});
+
+gulp.task("createWiki", ["createMenu"], function () {
+
+})
+
+
+gulp.task("createMenu", ["createMds"], function () {
+
+})
+
+gulp.task("createMds", function () {
 
     return gulp.src(caminhos)
         .pipe(through.obj((chunk, enc, cb) => {
             converter.convertWordToMd(chunk.history[0]);
             cb(null, chunk)
         }));
-});
+})
