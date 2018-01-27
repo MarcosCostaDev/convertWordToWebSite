91869d201bbbc36ff124021ae37b750dbbcbaaf2
Marcos Costa <marcos.lcosta@live.com>
2018-01-27 19:08:26 -0300

    vizualização de de menu melhorada

--- app/menuMain.js
+++ app/menuMain.js
@@ -13,6 +13,7 @@ function prepareMenu(mdPath) {
 
     itensMenuTemp.push({
         parent: path.dirname(mdPath),
+        folderParent: path.dirname(path.dirname(mdPath)),
         fileName: path.basename(fileMd),
         fullpath: mdPath,
         href: "/md/" + mdPath.replace(pathBase, "").replace(/\\/g, "/")
@@ -24,19 +25,30 @@ function generateMenu() {
 
     let itens = Enumerable.from(itensMenuTemp)
         .groupBy(p => p.parent)
+        .orderBy(g => g.key())
         .select((g, i) => {
             let parent = path.basename(g.key());
-            let itemTemp = `\n\n[${parent}]()\n\n`;
-            let texto = g.aggregate(( prev, atual) => {
+            let folderParent = g.first().folderParent;
+            let itemTemp = "";
+            if(path.resolve(folderParent) == path.resolve(globalConfig.configuracao.mdDist))
+            {
+                itemTemp = `\n\n[${parent}]()\n\n`;
+            }
+            else
+            {
+                itemTemp = `\n - - - - \n* # ${parent}\n\n`;
+            }
+            
+            let texto = g.aggregate((prev, atual) => {
                 itemTemp += `* [${atual.fileName.replace(".md", "")}](${atual.href}) \n`;
                 return itemTemp;
             });
-            return texto;     
+            return texto;
 
         }).toArray();
 
     for (let item of itens) {
-       fs.appendFileSync(`${path.resolve(globalConfig.configuracao.dest)}\\navigation.md`, item, "utf-8");
+        fs.appendFileSync(`${path.resolve(globalConfig.configuracao.dest)}\\navigation.md`, item, "utf-8");
 
     }
 
